bf5df7720c832ec46b78316a01d9151af154e891
Gregory Baker <gregory.baker@duke.edu>
2024-11-14 15:48:45 -0500

    Sensor Data Endpoint (IE-132)
    Motivation
    ---
    We want an endpoint for receiving sensor data. To start, we're just going to echo back the data sent to the endpoint.
    
    Modifications
    ---
    - Updated endpoint config
    - Echo back input

--- main.go
+++ main.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/config"
 )
 
-type MyEvent struct {
+type SensorEvent struct {
 	// sample properties below
-	InstanceId string `json:"InstanceId"`
-	Status     string `json:"Status"`
+	SensorType string            `json:"sensortype"`
+	SensorId   int16             `json:"sensorid"`
+	SensorData map[string]string `json:"sensordata"`
 }
 
-func HandleRequest(context context.Context, ev MyEvent) (events.APIGatewayProxyResponse, error) {
+func HandleRequest(context context.Context, ev events.APIGatewayV2HTTPRequest) (events.APIGatewayProxyResponse, error) {
 
 	res := events.APIGatewayProxyResponse{
 		IsBase64Encoded: false,
@@ -23,7 +25,26 @@ func HandleRequest(context context.Context, ev MyEvent) (events.APIGatewayProxyR
 		Body:            "Success!",
 	}
 
-	config.LoadDefaultConfig(context)
+	data := SensorEvent{}
+	err := json.Unmarshal([]byte(ev.Body), &data)
+
+	if err != nil {
+
+		res.StatusCode = 500
+		res.Body = err.Error()
+
+		return res, err
+	}
+
+	echo := fmt.Sprintf("Received data for SensorType %s and Id %d\r\n", data.SensorType, data.SensorId)
+
+	for key, val := range data.SensorData {
+		echo += fmt.Sprintf("Received Data %s with Value %s\r\n", key, val)
+	}
+
+	res.Body = echo
+
+	// config.LoadDefaultConfig(context)
 
 	return res, nil
 }
